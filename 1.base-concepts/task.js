d93e2185d2ed3319efb8ce2d636c22c249e63794
Ishchenko Evgenii <Evgenii.Ishchenko@yandex.ru>
2023-11-07 10:32:44 +0300

    homework 1.base-concepts is complete

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,20 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let discriminant = b**2 - 4*a*c;
+  if (discriminant === 0) {
+    arr.push(-b / 2 * a);
+  } else if (discriminant > 0) {
+    arr.push( (-b + Math.sqrt(discriminant)) / 2*a);
+    arr.push( (-b - Math.sqrt(discriminant)) / 2*a);
+  }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  percent = percent / 100 / 12;
+  let mortgageBody = amount - contribution;
+  let monthlyPayment = mortgageBody * (percent + (percent / (((1 + percent)**countMonths) - 1)));
+  let summ = monthlyPayment * countMonths;
+  return +summ.toFixed(2);
 }
